f31cfd76b73e3e083430d56683906844d9a36729
Emma Perkins <eperkins@seek.com.au>
2023-05-08 11:49:07 +1000

    Letters fix

--- src/Commands/RemainingLetters/remainingLetters.ts
+++ src/Commands/RemainingLetters/remainingLetters.ts
@@ -1,7 +1,7 @@
 import { getGuesses } from '../DynamoDB/Guess/guess';
 
-let remaining = 'a b c d e f g h i j k l m n o p q r s t u v w x y z ';
 export const remainingLetters = async (user: string, timeStamp: string) => {
+  let remaining = 'a b c d e f g h i j k l m n o p q r s t u v w x y z ';
   const guesses = await getGuesses(user, timeStamp);
   guesses.forEach((attempt) => {
     for (const char of attempt.guess) {
